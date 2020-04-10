724ce7da990c8cec609b437b2cb7d4b88d87657a
youliso <youliso@hotmail.com>
2020-04-10 13:28:50 +0800

    init

---
+++ src/test/test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+var (
+	v1 = Vertex{1, 2}  // 类型为 Vertex
+	v2 = Vertex{X: 1}  // Y:0 被省略
+	v3 = Vertex{}      // X:0 和 Y:0
+	p  = &Vertex{1, 2} // 类型为 *Vertex
+)
+
+func main() {
+	a := make([]int, 5)
+	printSlice("a", a)
+	b := make([]int, 0, 5)
+	printSlice("b", b)
+	c := b[:2]
+	printSlice("c", c)
+	d := c[2:5]
+	printSlice("d", d)
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
+}
